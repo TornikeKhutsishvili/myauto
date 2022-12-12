960de4c20621aafbbec501402610c9f9389bd37b
TornikeKhutsishvili <98366397+TornikeKhutsishvili@users.noreply.github.com>
2022-12-13 00:57:55 +0400

    Add files via upload

--- car1.js
+++ car1.js
@@ -1,13 +1,30 @@
 
+// მანქანები დაემატა მწარმოებლის, მოდელის, კატეგორიის(ჯიპი/სედანი), ფასის, გამოშვების წლის, სერიის.
+// დამატებული მანქანები გამოჩნდება ქარდბეის სახით როგორიც არის myuato-ზე.
+// ვამატებთ ძებნას. ავირჩევთ იმას თუ რას ვეძებთ და შესაბამიად გამოდის პასუხები. შეიძლება არჩეული 
+// იყოს ყველა პარამეტრი, რამდენიმე პარამეტრი ან სულაც ერთი.
+// მანქანები ასევე დაემატება გარბენის, გადაცემათა კოლოფის, საწვავის ტიპის, საჭის მდებარეობის მანქანაში და 
+// ადგილმდებარობის მხრივ. ასევე დავამატოთ თუ რამდენი წუთის წინ არის განცხადება განტავსებული და ასევე რამდენმა
+// ნახა ეს კონკრეტული განცხადება.
+
 class Car{
-    constructor(manufacturer,model,category,price,engine,year,newimage){
+    constructor(manufacturer,model,category,price,engine,year,mileage,transmission,
+                Fuel_Type,Wheel_main,locations,newimage,deletee,count){
         this.manufacturer = manufacturer
         this.model = model
         this.category = category
         this.price = price
         this.engine = engine
         this.year = year
+        this.mileage = mileage
+        this.transmission = transmission
+        this.Fuel_Type = Fuel_Type
+        this.Wheel_main = Wheel_main
+        this.locations = locations
         this.newimage = newimage
+        this.time = new Date().getTime()
+        this.deletee = deletee
+        this.count = count
     }
 }
 
@@ -20,8 +37,12 @@ var category = document.querySelector(".category")
 var price = document.querySelector(".price")
 var engine = document.querySelector(".engine")
 var year = document.querySelector(".year")
+var mileage = document.querySelector(".mileage")
+var transmission = document.querySelector(".transmission")
+var Fuel_Type = document.querySelector(".Fuel_Type")
+var Wheel_main = document.querySelector(".Wheel_main")
+var locations = document.querySelector(".locations")
 var body = document.querySelector("body")
-
 
 var image = document.querySelector("#image");
 var myimage = document.querySelector("#myimage")
@@ -31,105 +52,61 @@ image.addEventListener("change", function(){
     reader.addEventListener("load", () => {
         newimage = reader.result;
         myimage.style.backgroundImage = `url(${newimage})`
-        localStorage.setItem("cardizainfull",JSON.stringify(fullarr))
     });
     reader.readAsDataURL(this.files[0]);
 });
 
+var arr = [
+    new Car("Mercedes", "E-class", "სედანი"),
+    new Car("Mercedes", "S-class", "სედანი"),
+    new Car("Mercedes", "GL-class", "ჯიპი"),
+    new Car("BMW", "3er", "სედანი"),
+    new Car("BMW", "5er", "სედანი"),
+    new Car("BMW", "7er", "სედანი"),
+    new Car("Audi", "A7", "სედანი"),
+    new Car("Audi", "A8", "სედანი"),
+    new Car("Audi", "Q7", "ჯიპი"),
+    new Car("Toyota", "Prado", "ჯიპი"),
+    new Car("Toyota", "Camry", "სედანი"),
+    new Car("Toyota", "4Runner", "ჯიპი")
+];
+
+var manufacturerarr = new Set([...arr.map(i=> i.manufacturer)])
+for(var i of manufacturerarr){
+    var manufacturer_opt = document.createElement("option")
+    manufacturer_opt.innerText = i
+    manufacturer.appendChild(manufacturer_opt)
+}
+manufacturer.addEventListener("change", function(){
+    model.innerText = ''
+    var modelarr = new Set([...arr.filter(i=> i.manufacturer == manufacturer.value).map(o=> o.model)])
+    for(var i of modelarr){
+        var model_opt = document.createElement("option")
+        model_opt.innerText = i
+        model.appendChild(model_opt)
+    }
+});
+model.addEventListener("change", function(){
+    category.innerHTML = ''
+    var categoryarr = new Set([...arr.filter(i=> i.model == model.value).map(o=> o.category)])
+    for(var i of categoryarr){
+        var category_opt = document.createElement("option")
+        category_opt.innerText = i
+        category.appendChild(category_opt)
+    }
+});
+
 
 bottomsave.addEventListener("click", function(){
-    var obj = new Car(manufacturer.value,model.value,category.value,price.value,engine.value,year.value,newimage)
-
-    // jeeps
-    // mercedes jeeps
-    if(obj.manufacturer == "mercedes" && obj.category == "jeep" && obj.model == "GL-class"){
+    var obj = new Car(manufacturer.value,model.value,category.value,price.value,engine.value,year.value,mileage.value,
+                    transmission.value,Fuel_Type.value,Wheel_main.value,locations.value,newimage,this.time,this.deletee,this.count)
+    if(manufacturer.value != '' && model.value != '' && category.value != '' && price.value != '' && engine.value != '' 
+       && year.value != ''){
         fullarr.push(obj)
         localStorage.setItem("cardizainfull",JSON.stringify(fullarr))
     }
-    else if(obj.manufacturer == "mercedes" && obj.category != "jeep" && obj.model == "GL-class"){
+    else{
         console.log("wrong")
-    }
-
-    // audi jeeps
-    else if(obj.manufacturer == "audi" && obj.category == "jeep" && obj.model == "Q7"){
-        fullarr.push(obj)
-        localStorage.setItem("cardizainfull",JSON.stringify(fullarr))
-    }
-    else if(obj.manufacturer == "audi" && obj.category != "jeep" && obj.model == "Q7"){
-        console.log("wrong")
-    }
-
-    // toyota jeeps
-    else if(obj.manufacturer == "toyota" && obj.category == "jeep" && obj.model == "Prado" || obj.model == "4Runner"){
-        fullarr.push(obj)
-        localStorage.setItem("cardizainfull",JSON.stringify(fullarr))
-    }
-    else if(obj.manufacturer == "toyota" && obj.category != "jeep" && obj.model == "Prado" || obj.model == "4Runner"){
-        console.log("wrong")
-    }
-
-    // sedan
-    // mercedes sedan
-    else if(obj.manufacturer == "mercedes" && obj.category == "sedan" && obj.model == "E-class" || obj.model == "S-class"){
-        fullarr.push(obj)
-        localStorage.setItem("cardizainfull",JSON.stringify(fullarr))
-    }
-    else if(obj.manufacturer == "mercedes" && obj.category != "sedan" && obj.model == "E-class" || obj.model == "S-class"){
-        console.log("wrong")
-    }
-
-    // toyota sedan
-    else if(obj.manufacturer == "toyota" && obj.category == "sedan" && obj.model == "Camry"){
-        fullarr.push(obj)
-        localStorage.setItem("cardizainfull",JSON.stringify(fullarr))
-    }
-    else if(obj.manufacturer == "toyota" && obj.category != "sedan" && obj.model == "Camry"){
-        console.log("wrong")
-    }
-
-    // audi sedan
-    else if(obj.manufacturer == "audi" && obj.category == "sedan" && obj.model == "A7" || obj.model == "A8"){
-        fullarr.push(obj)
-        localStorage.setItem("cardizainfull",JSON.stringify(fullarr))
-    }
-    else if(obj.manufacturer == "audi" && obj.category != "sedan" && obj.model == "A7" || obj.model == "A8"){
-        console.log("wrong")
-    }
-
-    // bmw sedan
-    else if(obj.manufacturer == "bmw" && obj.category == "sedan" && obj.model == "3er" || obj.model == "5er" || obj.model == "7er"){
-        fullarr.push(obj)
-        localStorage.setItem("cardizainfull",JSON.stringify(fullarr))
-    }
-    else if(obj.manufacturer == "bmw" && obj.category != "sedan" && obj.model == "3er" || obj.model == "5er" || obj.model == "7er"){
-        console.log("wrong")
-    }
-
-    // mercedes
-    else if(obj.manufacturer == "mercedes" && obj.model == "E-class" || obj.model == "S-class" || obj.model == "GL-class"){
-        fullarr.push(obj)
-        localStorage.setItem("cardizainfull",JSON.stringify(fullarr))
-    }
-    else if(obj.manufacturer == "mercedes" && obj.model != "E-class" || obj.model == "S-class" || obj.model == "GL-class"){
-        console.log("wrong")
-    }
-
-    // bmw
-    else if(obj.manufacturer == "bmw" && obj.model == "3er" || obj.model == "5er" || obj.model == "7er"){
-        fullarr.push(obj)
-        localStorage.setItem("cardizainfull",JSON.stringify(fullarr))
-    }
-
-    // audi
-    else if(obj.manufacturer == "audi" && obj.model == "A7" || obj.model == "A8" || obj.model == "Q7"){
-        fullarr.push(obj)
-        localStorage.setItem("cardizainfull",JSON.stringify(fullarr))
-    }
-
-    // toyota
-    else if(obj.manufacturer == "toyota" && obj.model == "Prado" || obj.model == "Camry" || obj.model == "4Runner"){
-        fullarr.push(obj)
-        localStorage.setItem("cardizainfull",JSON.stringify(fullarr))
     }
 });
 console.log(fullarr)
--- car2.js
+++ car2.js
@@ -1,61 +1,68 @@
 
-var filtermanufactorer = document.querySelector(".filtermanufactorer")
-var filtermodel = document.querySelector(".filtermodel")
-var filterCategory = document.querySelector(".filterCategory")
-var fromyears = document.querySelector(".fromyears")
-var toyears = document.querySelector(".toyears")
-var fromprice = document.querySelector(".fromprice")
-var toprice = document.querySelector(".toprice")
-var fromengine = document.querySelector(".fromengine")
-var toengine = document.querySelector(".toengine")
-var body = document.querySelector("body")
-
+// მანქანები დაემატა მწარმოებლის, მოდელის, კატეგორიის(ჯიპი/სედანი), ფასის, გამოშვების წლის, სერიის.
+// დამატებული მანქანები გამოჩნდება ქარდბეის სახით როგორიც არის myuato-ზე.
+// ვამატებთ ძებნას. ავირჩევთ იმას თუ რას ვეძებთ და შესაბამიად გამოდის პასუხები. შეიძლება არჩეული 
+// იყოს ყველა პარამეტრი, რამდენიმე პარამეტრი ან სულაც ერთი.
+// დავამატოთ მანქანების ქარდებზე ერთი delete ღილაკი. ღილაკზე კლიკის შემთხვევაში მანქანა წაიშალოს სიიდან. 
+// არ იქნება არც ვიზუალზე და არც მასივში.
 
 var fullarr = JSON.parse(localStorage.getItem("cardizainfull"))
+var body = document.querySelector("body")
+
 for(var i of fullarr){
-    let tmp = `
-    <div class="cardiv">
-    <div class="cardiv2">
-      <div class="carphoto style="background-image: url(${i.newimage})">
+  var hourgone = parseInt(((new Date().getTime() - i.time)/1000)/60/60)
+  // if(hourgone >= 60){
+  //   console.log(hourgone + " " + "საათის წინ")
+  // }
+  
+  // var minutegone = parseInt(((new Date().getTime() - i.time)/1000)/60)
+  // if(minutegone < 60){
+  //   console.log(minutegone + " " + "წუთის წინ")
+  // }
+
+  let tmp = `
+  <div class="cardiv">
+    <div class="cardiv2" onclick="see()">
+      <div class="carphoto" style="background-image: url(${i.newimage})">      
+      </div>
         <div class="model_years">
         <div>${i.manufacturer}</div> <div>${i.model}</div> <div>${i.year} წ</div>
         </div>
         <div class="engine">
           <div class="enginesharp">
             <div class="">${i.engine}</div>
-            <h1 class="enginesharph1">ბენზინი</h1>
+            <h1 class="enginesharph1">${i.Fuel_Type}</h1>
           </div>
           <div class="gear">
             <div class=""><i class="fa-solid fa-gear"></i></div>
-            <h1 class="gearh1">ტიპტრონიკი</h1>
+            <h1 class="gearh1">${i.transmission}</h1>
           </div>
-        </div>
-        <div class="">
-
         </div>
         <div class="see">
           <div class="vipdiv">S-VIP</div>
-          <div class="">458 ნახვა</div>
-          <div class="">3 წუთის წინ</div>
+          <div class="">${i.count} ნახვა</div>
+          <div class="">
+              ${hourgone + " " + "საათის წინ"}
+          </div>
+        </div>
+        <div class="deldiv">
+          <button class="dlt">წაშლა</button>
         </div>
         <div class="km">
           <div class="speed">
             <div class=""><i class="fa-solid fa-gauge"></i></div>
-            <h1 class="speedh1">165800 კმ</h1>
+            <h1 class="speedh1">${i.mileage} კმ</h1>
           </div>
           <div class="whell">
             <div class=""><i class="fa-solid fa-dharmachakra"></i></div>
-            <h1 class="whellh1">მარცხენა</h1>
+            <h1 class="whellh1">${i.Wheel_main}</h1>
           </div>
         </div>
         <div class="border_price">
           <h1 class="h1red">განბაჟება 2,345 ₾</h1>
-          <h1 class="h1dark">თბილისი</h1>
+          <h1 class="h1dark">${i.locations}</h1>
         </div>
         <div class="price">
-          <div>
-
-          </div>
           <div class="valut">
             <div class="cash">${i.price}</div>
             <div class="wallut">
@@ -69,7 +76,6 @@ for(var i of fullarr){
           <i class="fa-solid fa-location-dot"></i>
           <i class="fa-regular fa-heart"></i>
         </div>
-      </div>
     </div>
   </div>
   `
@@ -77,74 +83,166 @@ for(var i of fullarr){
 }
 console.log(fullarr)
 
+class CarInfo{
+  constructor(manufacturer,model,category,fromprice,toprice,fromengine,toengine,fromyears,toyears,frommileage,tomileage,
+              transmission,Fuel_Type,Wheel_main,locations){
+    this.manufacturer = manufacturer
+    this.model = model
+    this.category = category
+    this.fromprice = fromprice
+    this.toprice = toprice
+    this.fromengine = fromengine
+    this.toengine = toengine
+    this.fromyears = fromyears
+    this.toyears = toyears
+    this.frommileage = frommileage
+    this.tomileage = tomileage
+    this.transmission = transmission
+    this.Fuel_Type = Fuel_Type
+    this.Wheel_main = Wheel_main
+    this.locations = locations
+  }
+}
 
-// btnsearch.addEventListener("click", function(){
-//   for(var i of fullarr){
-//     if(i.manufactorer == "mercedes" && i.category == "jeep" && i.model == "GL-class"){
-//       fullarr.sort(function(){
-//         body.innerHTML += fullarr
-//       });
-//           var tmp1 = `
-//           <div class="cardiv">
-//           <div class="cardiv2">
-//             <div class="carphoto">
-//               <div class="model_years">
-//               <div>${i.manufacturer}</div> <div>${i.model}</div> <div>${i.year} წ</div>
-//               </div>
-//               <div class="engine">
-//                 <div class="enginesharp">
-//                   <div class="">${i.engine}</div>
-//                   <h1 class="enginesharph1">ბენზინი</h1>
-//                 </div>
-//                 <div class="gear">
-//                   <div class=""><i class="fa-solid fa-gear"></i></div>
-//                   <h1 class="gearh1">ტიპტრონიკი</h1>
-//                 </div>
-//               </div>
-//               <div class="">
-        
-//               </div>
-//               <div class="see">
-//                 <div class="vipdiv">S-VIP</div>
-//                 <div class="">458 ნახვა</div>
-//                 <div class="">3 წუთის წინ</div>
-//               </div>
-//               <div class="km">
-//                 <div class="speed">
-//                   <div class=""><i class="fa-solid fa-gauge"></i></div>
-//                   <h1 class="speedh1">165800 კმ</h1>
-//                 </div>
-//                 <div class="whell">
-//                   <div class=""><i class="fa-solid fa-dharmachakra"></i></div>
-//                   <h1 class="whellh1">მარცხენა</h1>
-//                 </div>
-//               </div>
-//               <div class="border_price">
-//                 <h1 class="h1red">განბაჟება 2,345 ₾</h1>
-//                 <h1 class="h1dark">თბილისი</h1>
-//               </div>
-//               <div class="price">
-//                 <div>
-        
-//                 </div>
-//                 <div class="valut">
-//                   <div class="cash">${i.price}</div>
-//                   <div class="wallut">
-//                     <div class="dollar">$</div>
-//                     <div class="lari">₾</div>
-//                   </div>
-//                 </div>
-//               </div>
-//               <div class="favourite">
-//                 <i class="fa-sharp fa-solid fa-message"></i>
-//                 <i class="fa-solid fa-location-dot"></i>
-//                 <i class="fa-regular fa-heart"></i>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//         `
-//         body.innerHTML += tmp1
-//     }
-//  }
-// });
+var manufacturer = document.querySelector(".manufactorer")
+var model = document.querySelector(".model")
+var category = document.querySelector(".category")
+var fromprice = document.querySelector(".fromprice")
+var toprice = document.querySelector(".toprice")
+var fromengine = document.querySelector(".fromengine")
+var toengine = document.querySelector(".toengine")
+var fromyears = document.querySelector(".fromyears")
+var toyears = document.querySelector(".toyears")
+var frommileage = document.querySelector(".frommileage")
+var tomileage = document.querySelector(".tomileage")
+var transmission = document.querySelector(".transmission")
+var Fuel_Type = document.querySelector(".Fuel_Type")
+var Wheel_main = document.querySelector(".Wheel_main")
+var locations = document.querySelector(".locations")
+
+var manufacturerarr = new Set([...fullarr.map(i=> i.manufacturer)])
+for(var i of manufacturerarr){
+    var manufactureropt = document.createElement("option")
+    manufactureropt.innerText = i
+    manufacturer.appendChild(manufactureropt)
+}
+manufacturer.addEventListener("click", function(){
+    model.innerText = ''
+    var modelarr = new Set([...fullarr.filter(i=> i.manufacturer == manufacturer.value).map(o=> o.model)])
+    for(var i of modelarr){
+        var model_opt = document.createElement("option")
+        model_opt.innerText = i
+        model.appendChild(model_opt)
+    }
+});
+model.addEventListener("change", function(){
+  category.innerHTML = ''
+    var categoryarr = new Set([...fullarr.filter(i=> i.model == model.value).map(o=> o.category)])
+    for(var i of categoryarr){
+        var category_opt = document.createElement("option")
+        category_opt.innerText = i
+        category.appendChild(category_opt)
+    }
+});
+
+var btnsearch = document.querySelector(".btnsearch")
+btnsearch.addEventListener("click", function(){
+  body.innerHTML = ""
+  for(var i of fullarr){
+    var minprice = fromprice.value
+    var maxprice = toprice.value
+    var minyears = fromyears.value
+    var maxyears = toyears.value
+    var minengine = fromengine.value
+    var maxengine = toengine.value
+
+    if(i.price >= minprice && maxprice >= i.price && i.year >= minyears && maxyears >= i.year 
+       && i.engine >= minengine && maxengine >= i.engine){
+      var tmp = `
+      <div class="cardiv">
+        <div class="cardiv2" onclick="see()">
+          <div class="carphoto" style="background-image: url(${i.newimage})">      
+          </div>
+            <div class="model_years">
+            <div>${i.manufacturer}</div> <div>${i.model}</div> <div>${i.year} წ</div>
+            </div>
+            <div class="engine">
+              <div class="enginesharp">
+                <div class="">${i.engine}</div>
+                <h1 class="enginesharph1">${i.Fuel_Type}</h1>
+              </div>
+              <div class="gear">
+                <div class=""><i class="fa-solid fa-gear"></i></div>
+                <h1 class="gearh1">${i.transmission}</h1>
+              </div>
+            </div>
+            <div class="see">
+              <div class="vipdiv">S-VIP</div>
+              <div class="">${i.count} ნახვა</div>
+              <div class="">${timegone} წუთის წინ</div>
+            </div>
+            <div class="deldiv">
+              <button class="dlt">წაშლა</button>
+            </div>
+            <div class="km">
+              <div class="speed">
+                <div class=""><i class="fa-solid fa-gauge"></i></div>
+                <h1 class="speedh1">${i.mileage} კმ</h1>
+              </div>
+              <div class="whell">
+                <div class=""><i class="fa-solid fa-dharmachakra"></i></div>
+                <h1 class="whellh1">${i.Wheel_main}</h1>
+              </div>
+            </div>
+            <div class="border_price">
+              <h1 class="h1red">განბაჟება 2,345 ₾</h1>
+              <h1 class="h1dark">${i.locations}</h1>
+            </div>
+            <div class="price">
+              <div class="valut">
+                <div class="cash">${i.price}</div>
+                <div class="wallut">
+                  <div class="dollar">$</div>
+                  <div class="lari">₾</div>
+                </div>
+              </div>
+            </div>
+            <div class="favourite">
+              <i class="fa-sharp fa-solid fa-message"></i>
+              <i class="fa-solid fa-location-dot"></i>
+              <i class="fa-regular fa-heart"></i>
+            </div>
+        </div>
+      </div>
+      `
+      body.innerHTML += tmp
+    }
+    else if(i.price <= minprice && maxprice <= i.price && i.year <= minyears && maxyears <= i.year 
+            && i.engine <= minengine && maxengine <= i.engine){
+      console.log("wrong")
+    }
+  }
+});
+
+function see(){
+  for(var i of fullarr){
+      i.count += 1
+      localStorage.setItem("cardizainfull",JSON.stringify(fullarr))
+  }
+}
+
+const dlt = document.querySelectorAll(".dlt")
+let cardiv = document.querySelectorAll(".cardiv")
+cardiv = [...cardiv]
+
+for(var i=0; i<dlt.length; i++){
+  dlt[i].addEventListener("click", function(){
+    // fullarr.splice(i,1)
+    // [...dlt].indexOf(this)
+    console.log([...dlt].indexOf(this))
+    // console.log([...cardiv].indexOf(this))
+    cardiv[i].classList.add("none");
+    // console.log(body[cardiv].classList.add("none"))
+    localStorage.setItem("cardizainfull", JSON.stringify(fullarr))
+  });
+}
